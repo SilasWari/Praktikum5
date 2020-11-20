b74144f2d77ea9afb4079980efc5c29ee322184c
Silas Wari <warisila@hs-furtwangen.de>
2020-11-20 22:11:55 +0100

    Erster Teil Aufgabenblatt 6: Queue und Util Klasse hinzugefuegt und UtilTest Klasse erstellt.

--- Praktikum5Wari/src/main/java/de/hfu/App.java
+++ Praktikum5Wari/src/main/java/de/hfu/App.java
@@ -1,6 +1,12 @@
 package de.hfu;
 import java.util.Scanner;
 public class App {
+	
+	/**
+	 * Changes lowercase letters to uppercase letters 
+	 * @param args
+	 */
+	
     public  static void main(String args[]){
         Scanner sc = new Scanner(System.in);
         System.out.println("Eingabe:");
